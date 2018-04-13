bcd20952ccd26929aec433503e449361732e9ccc
Mavey Ma <mma@csumb.edu>
2018-04-13 20:31:29 +0000

    Tested Ajax calls for Remove, Add, isTeamManager

--- scripts/AjaxTest.jsx
+++ scripts/AjaxTest.jsx
@@ -16,39 +16,39 @@ class AjaxTest extends React.Component {
 
     
       componentDidMount() {
-         $.ajax({
+      //   $.ajax({
         
-        type: "GET",
-        url: "../API/DisplayUsers.php",
-        dataType: "json",
-        data: { "EmployeeID": 11 },
-        success: function(data,status) {
-           for(var i=0; i<data.length;i++){
-               $("#Name").append("<option>" + data[i].Name + "</option>");
-           }
+      //   type: "GET",
+      //   url: "../API/DisplayUsers.php",
+      //   dataType: "json",
+      //   data: { "EmployeeID": 11 },
+      //   success: function(data,status) {
+      //     for(var i=0; i<data.length;i++){
+      //         $("#Name").append("<option>" + data[i].Name + "</option>");
+      //     }
         
-        },
-        complete: function(data,status) { //optional, used for debugging purposes
-        //alert(status);
-        }
+      //   },
+      //   complete: function(data,status) { //optional, used for debugging purposes
+      //   //alert(status);
+      //   }
         
-        });//ajax
+      //   });//ajax
         
-       $.ajax({
+      // $.ajax({
         
-        type: "GET",
-        url: "../API/ChangeIsManager.php",
-        dataType: "json",
-        data: { "EmployeeID": 11,
-                "isManager": 0},
-        success: function(data,status) {
-        alert("Success");
-        },
-        complete: function(data,status) { //optional, used for debugging purposes
-        //alert(status);
-        }
+      //   type: "GET",
+      //   url: "../API/ChangeIsManager.php",
+      //   dataType: "json",
+      //   data: { "EmployeeID": 11,
+      //           "isManager": 0},
+      //   success: function(data,status) {
+      //   alert("Success");
+      //   },
+      //   complete: function(data,status) { //optional, used for debugging purposes
+      //   //alert(status);
+      //   }
         
-        });//ajax
+      //   });//ajax
         
         $.ajax({
         
@@ -68,26 +68,26 @@ class AjaxTest extends React.Component {
         
         });//ajax
                  
-   /* adding to team             
-        $.ajax({
+   // adding to team             
+        // $.ajax({
         
-        type: "GET",
-        url: "../API/AddToTeam.php",
-        dataType: "json",
-        data: { "EmployeeID": 1,
-                "TeamID": 31,
-                "isTeamManager":0
-        },
-        success: function(data,status) {
+        // type: "GET",
+        // url: "../API/AddToTeam.php",
+        // dataType: "json",
+        // data: { "EmployeeID": 1,
+        //         "TeamID": 31,
+        //         "isTeamManager":0
+        // },
+        // success: function(data,status) {
 
-        },
-        complete: function(data,status) { //optional, used for debugging purposes
-        //alert(status);
-        }
+        // },
+        // complete: function(data,status) { //optional, used for debugging purposes
+        // //alert(status);
+        // }
       
-        });//ajax
-        */
-         $.ajax({
+        // });//ajax
+        
+        $.ajax({
         
         type: "GET",
         url: "../API/RemoveFromTeam.php",
