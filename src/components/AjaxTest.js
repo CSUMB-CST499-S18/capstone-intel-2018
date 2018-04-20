0ec17782aaa55f4aab826d6467d9ec55c13a8adf
Brandon <bavery@csumb.edu>
2018-04-20 20:00:04 +0000

    Debugging display user api call

--- src/components/AjaxTest.js
+++ src/components/AjaxTest.js
@@ -18,7 +18,7 @@ class AjaxTest extends React.Component {
   blah() {
     $.ajax({
       type: "GET",
-      url: process.env.IP + "../../API/DisplayUsers.php",
+      url: "https://cst499s18-bavery.c9users.io:8080/Project_Folder/capstone-intel-2018/API/DisplayUsers.php",
       dataType: "json",
       data: { "EmployeeID": 11 },
       success: function(data,status) {
