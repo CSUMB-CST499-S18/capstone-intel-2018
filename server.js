0a9d0a46b420aeb269c2f7882a3ac3aeb17c5f12
Jaime Velazquez <javelazquez@csumb.edu>
2018-04-26 12:19:41 +0000

    added dist/API filepath express.static

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 app.use(express.static(path.join(__dirname, '/dist')));
-app.use('/dist', express.static(__dirname + '/API'));
+app.use('/API', express.static(__dirname + '/dist'));
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/dist/index.html')
