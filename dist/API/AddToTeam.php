b284eb876cc8671fad052e585283f1c1e183e35b
Jaime Velazquez <javelazquez@csumb.edu>
2018-05-08 03:27:53 +0000

    Implemented add/remove employee from team as member/manager to the DB

--- dist/API/AddToTeam.php
+++ dist/API/AddToTeam.php
@@ -1,23 +1,218 @@
+
 <?php
+//CODE NEEDS TO BE REFACTORED
+
+// WORKFLOW
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// IF EMPLOYEE WILL BE THE OWNER
+// Grab all children of TeamID.
+// Grab all ResourceID's attached to those childrens TeamIDs + original TeamID.
+// Insert all instances where ResourceID AND EmployeeID appear in employeeresource table.
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// IF NOT THE OWNER
+// No need to lookup children
+// Grabs all resourceID's based on TeamID
+// Insert all instances where ResourceID AND EmployeeID appear in employeeresource table.
+
+//////////////////////////////////////////////////////////////////////////////////////////
 header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN'] . "");
 include 'dbConnection.php';
 
 
 $conn = getDatabaseConnection();
 
-$sql = "INSERT INTO employeeteam
+if($_GET['isTeamManager'] == 1){ //If they're a manager, get child teams
+        $sql = "SELECT ChildTeams
+        FROM parentteam
+        WHERE ParentTeam = :ParentTeam";
+
+        $namedParameters = array();
+        $namedParameters[':ParentTeam'] = $_GET['TeamID'];
+        
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($namedParameters);
+        $record = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        $record = explode(",", $record["ChildTeams"]); //stores all child teams in an array
+         
+        /* Here we loop through the array and append all child teams to the query */
+                
+        $sql = "SELECT ResourceID FROM teamresource WHERE";
+                
+        for($i = 0; $i < count($record); $i++){
+                $sql = $sql . " TeamID = " . $record[$i];
+                if($i < count($record) - 1) {
+                        $sql = $sql . " OR ";
+                        }
+                }
+        //$sql = $sql . " OR TeamID = " . $_GET['TeamID'];
+        
+        /* Here insert all ResourceID's into the $rows array*/
+                
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($namedParameters);
+        $resourceID = array();
+        
+        while( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+                $resourceID[] = $row; // appends each row to the array. 
+        }
+
+        /* adds VISITOR permissions to child teams */        
+        $sql = "INSERT INTO employeeresource ( EmployeeID, ResourceID, AccessID )
+        VALUES ";
+        
+        for($i = 0; $i < count($resourceID); $i++){
+        $sql = $sql . "(:EmployeeID, " . $resourceID[$i]["ResourceID"] . " , " . 3 . "),";
+        if($i == count($resourceID) - 1) {
+        $sql = substr($sql, 0, -1);
+        $sql = $sql . ";}";
+                }
+        }
+        $namedParameters = array();
+        $namedParameters[':EmployeeID'] = $_GET['EmployeeID'];
+        
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($namedParameters);
+         
+        /* Adds OWNER access to team being assigned */
+         
+        $sql = "SELECT ResourceID FROM teamresource WHERE TeamID = :TeamID";
+        
+        $namedParameters = array();
+        $namedParameters[':TeamID'] = $_GET['TeamID'];
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($namedParameters);
+        $resourceID = array();
+        
+        while( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+                $resourceID[] = $row; // appends each row to the array. 
+        }       
+                /* adds owner permissions for TeamID */        
+        $sql = "INSERT INTO employeeresource ( EmployeeID, ResourceID, AccessID )
+        VALUES ";
+        
+        for($i = 0; $i < count($resourceID); $i++){
+                $sql = $sql . "(:EmployeeID, " . $resourceID[$i]["ResourceID"] . " , " . 1 . "),";
+                if($i == count($resourceID) - 1) {
+                $sql = substr($sql, 0, -1);
+                $sql = $sql . ";}";
+                }
+        }
+        $namedParameters = array();
+        $namedParameters[':EmployeeID'] = $_GET['EmployeeID'];
+        
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($namedParameters);
+
+        
+        /******************************Adding the owner as a member to the PARENTNODE team as member **************************/
+        
+        /* Get the ParentNode of the team in which they are being made manager. */
+        
+        $sql = "SELECT ParentNode FROM parentteam WHERE ParentTeam = :TeamID";
+        
+        $namedParameters = array();
+        $namedParameters[':TeamID'] = $_GET['TeamID'];
+        
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($namedParameters);
+        $ParentNode = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        $sql = "SELECT ResourceID FROM teamresource WHERE TeamID = :TeamID";
+
+        $namedParameters = array();
+        $namedParameters[':TeamID'] = $ParentNode;        
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($namedParameters);
+        $resourceID = array();
+        
+        while( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+                $resourceID[] = $row; // appends each row to the array. 
+        }
+        
+        /* Grant employee member access to their new team */
+        $sql = "INSERT INTO employeeresource ( EmployeeID, ResourceID, AccessID )
+        VALUES " . "(:EmployeeID, " . $resourceID[1]["ResourceID"] . " , " . 2 . ");} ";
+
+        $namedParameters = array();
+        $namedParameters[':EmployeeID'] = $_GET['EmployeeID'];
+        
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($namedParameters);
+        
+        /****************************************Add employee to employeeteam table *********************************/
+        
+        /* Adds employee to PARENTNODE team*/
+        $sql = "INSERT INTO employeeteam
         (EmployeeID, TeamID, isTeamManager)
         VALUES( :EmployeeID, :TeamID, :isTeamManager)";
 
-//Sanitizing Input
-$namedParameters = array();
-$namedParameters[':EmployeeID'] = $_GET['EmployeeID'];
-$namedParameters[':TeamID'] = $_GET['TeamID'];
-$namedParameters[':isTeamManager'] = $_GET['isTeamManager'];
+        //Sanitizing Input
+        $namedParameters = array();
+        $namedParameters[':EmployeeID'] = $_GET['EmployeeID'];
+        $namedParameters[':TeamID'] = $ParentNode;
+        $namedParameters[':isTeamManager'] = 0;
 
-$stmt = $conn->prepare($sql);
-$stmt->execute($namedParameters);
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($namedParameters);
+        
+        /* Adds employee to employeeteam WHERE ISTEAMMANAGER = 1 */
+        $sql = "INSERT INTO employeeteam
+        (EmployeeID, TeamID, isTeamManager)
+        VALUES( :EmployeeID, :TeamID, :isTeamManager)";
 
+        //Sanitizing Input
+        $namedParameters = array();
+        $namedParameters[':EmployeeID'] = $_GET['EmployeeID'];
+        $namedParameters[':TeamID'] = $_GET['TeamID'];
+        $namedParameters[':isTeamManager'] = $_GET['isTeamManager'];
 
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($namedParameters);
+         
+} else { // else if the employee will not be the manager **************************************************************
 
+        /* Get resource ID's for that team ID */
+                
+        $sql = "SELECT ResourceID FROM teamresource WHERE TeamID = :TeamID";
+
+        $namedParameters = array();
+        $namedParameters[':TeamID'] = $_GET['TeamID'];        
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($namedParameters);
+        $resourceID = array();
+        
+        while( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+                $resourceID[] = $row; // appends each row to the array. 
+        }
+        
+        /* Grant employee member access to their new team */
+        $sql = "INSERT INTO employeeresource ( EmployeeID, ResourceID, AccessID )
+        VALUES " . "(:EmployeeID, " . $resourceID[1]["ResourceID"] . " , " . 2 . ");} ";
+
+        $namedParameters = array();
+        $namedParameters[':EmployeeID'] = $_GET['EmployeeID'];
+        
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($namedParameters);
+        
+        /* Adds employee to employeeteam */
+        $sql = "INSERT INTO employeeteam
+        (EmployeeID, TeamID, isTeamManager)
+        VALUES( :EmployeeID, :TeamID, :isTeamManager)";
+
+        //Sanitizing Input
+        $namedParameters = array();
+        $namedParameters[':EmployeeID'] = $_GET['EmployeeID'];
+        $namedParameters[':TeamID'] = $_GET['TeamID'];
+        $namedParameters[':isTeamManager'] = $_GET['isTeamManager'];
+
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($namedParameters);
+        
+        
+}        
 ?>
--- dist/API/RemoveFromTeam.php
+++ dist/API/RemoveFromTeam.php
@@ -1,4 +1,25 @@
 <?php
+//CODE NEEDS TO BE REFACTORED
+
+// WORKFLOW
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// IF EMPLOYEE IS THE OWNER
+// Grab all children of TeamID.
+// Grab all ResourceID's attached to those childrens TeamIDs + original TeamID.
+// Delete all instances where ResourceID AND EmployeeID appear in employeeresource table.
+// Lastly, delete employee from employeeteam table using EmployeeID and TeamID.
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// IF NOT THE OWNER
+// No need to lookup children
+// Grabs all resourceID's based on TeamID
+// Delete all instances where ResourceID AND EmployeeID appear in employeeresource table.
+// Lastly, delete employee from employeeteam table using EmployeeID and TeamID.
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
 header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN'] . "");
 include 'dbConnection.php';
 
@@ -7,36 +28,205 @@ $conn = getDatabaseConnection();
 //Get child teams
 $sql = "SELECT ChildTeams
         FROM parentteam
-        WHERE ParentTeam = :ParentTeam";
+        WHERE ParentTeam = :TeamID";
 
 $namedParameters = array();
-$namedParameters[':ParentTeam'] = $_GET['ParentTeam'];
+$namedParameters[':TeamID'] = $_GET['TeamID'];
         
 $stmt = $conn->prepare($sql);
 $stmt->execute($namedParameters);
 $record = $stmt->fetch(PDO::FETCH_ASSOC);
 
-echo json_encode($record);
+if (isset($record)){ //If communication with the DB was established
+/*Here we check if they are the manager of the team */
+        $sql = "SELECT isTeamManager
+        FROM employeeteam
+        WHERE TeamID = :TeamID
+        AND EmployeeID = :EmployeeID";
 
+        $namedParameters = array();
+        $namedParameters[':TeamID'] = $_GET['TeamID'];
+        $namedParameters[':EmployeeID'] = $_GET['EmployeeID'];
 
-/*$sql = "DELETE FROM employeeteam
-        WHERE EmployeeID  = :EmployeeID
-        AND TeamID = :TeamID";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($namedParameters);
+        $isTeamManager = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        //echo $isTeamManager["isTeamManager"];
+        
+        if($isTeamManager["isTeamManager"] == 1){
+                
+                $record = explode(",", $record["ChildTeams"]); //stores all child teams in an array
+                //echo $record[0];
+           /*
+        /* Here we loop through the array and append all child teams to the query */
+                
+                $sql = "SELECT ResourceID FROM teamresource WHERE";
+                
+                for($i = 0; $i < count($record); $i++){
+                        $sql = $sql . " TeamID = " . $record[$i];
+                        if($i < count($record) - 1) {
+                                $sql = $sql . " OR ";
+                        }
+                }
+                $sql = $sql . " OR TeamID = " . $_GET['TeamID'];
+                
+        /* Here insert all ResourceID's into the $rows array*/
+                
+                $stmt = $conn->prepare($sql);
+                $stmt->execute();
+                $resourceID = array();
+        
+                while( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+                        $resourceID[] = $row; // appends each row to the array. 
+                }
+                
+        /* Here we loop through the $rows array and append all ResourceID's to sql query, and delete from employeeresource*/
+                
+                $sql = "DELETE FROM employeeresource
+                        WHERE EmployeeID = :EmployeeID 
+                        AND (AccessID = 3 OR AccessID = 1) 
+                        AND (";
+                
+                for($i = 0; $i < count($resourceID); $i++){
+                        $sql = $sql . " ResourceID = " . $resourceID[$i]["ResourceID"];
+                        if($i < count($resourceID) - 1) {
+                                $sql = $sql . " OR ";
+                        } elseif($i == count($resourceID) - 1){
+                                $sql = $sql . ")";
+                        }
+                }
+                
+                $namedParameters = array();
+                $namedParameters[':EmployeeID'] = $_GET['EmployeeID'];
+        
+                $stmt = $conn->prepare($sql);
+                $stmt->execute($namedParameters);
+                
+/*****************************Deleting OWNER from their PARENTNODE team in which they are members. **************************/
 
-//Sanitizing Input
-$namedParameters = array();
-$namedParameters[':EmployeeID'] = $_GET['EmployeeID'];
-$namedParameters[':TeamID'] = $_GET['TeamID'];
+                /* Get the ParentNode of the team in which they are being made manager. */
+        
+                $sql = "SELECT ParentNode FROM parentteam WHERE ParentTeam = :TeamID";
+        
+                $namedParameters = array();
+                $namedParameters[':TeamID'] = $_GET['TeamID'];
+        
+                $stmt = $conn->prepare($sql);
+                $stmt->execute($namedParameters);
+                $ParentNode = $stmt->fetch(PDO::FETCH_ASSOC);
 
-$stmt = $conn->prepare($sql);
-$stmt->execute($namedParameters);
-*/
+                //Where employee is member of the team, delete resources from employeeresource
+                $sql = "SELECT ResourceID FROM teamresource WHERE TeamID = :TeamID";
+                
+                $namedParameters = array();
+                $namedParameters[':TeamID'] = $ParentNode;
+                $stmt = $conn->prepare($sql);
+                $stmt->execute($namedParameters);
+                $resourceID = array();
+        
+                while( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+                        $resourceID[] = $row; // appends each row to the array. 
+                }
+                
+                $sql = "DELETE FROM employeeresource
+                        WHERE EmployeeID = :EmployeeID 
+                        AND AccessID = 2 
+                        AND (";
+                
+                for($i = 0; $i < count($resourceID); $i++){
+                        $sql = $sql . " ResourceID = " . $resourceID[$i]["ResourceID"];
+                        if($i < count($resourceID) - 1) {
+                                $sql = $sql . " OR ";
+                        } elseif($i == count($resourceID) - 1){
+                                $sql = $sql . ")";
+                        }
+                }
+                
+                $namedParameters = array();
+                $namedParameters[':EmployeeID'] = $_GET['EmployeeID'];
+        
+                $stmt = $conn->prepare($sql);
+                $stmt->execute($namedParameters);
 
-//TODO: REMOVAL LOGIC
-//SELECT *
-//FROM teamresource NATURAL JOIN employeeresource 
-//WHERE employeeresource.ResourceID = teamresource.ResourceID AND employeeresource.EmployeeID = 1 AND TeamID = 21;
+/********************** Deleting EMPLOYEE from employeeteam table in which they are OWNER and MEMBER ****************************/
 
-//This removes an employees resource access to that specific team ID.
+                
+        /* Here we delete the employee from the employeeteam table WHERE THEY ARE OWNER */
+                $sql = "DELETE FROM employeeteam
+                        WHERE EmployeeID  = :EmployeeID
+                        AND TeamID = :TeamID";
+        
+                //Sanitizing Input
+                $namedParameters = array();
+                $namedParameters[':EmployeeID'] = $_GET['EmployeeID'];
+                $namedParameters[':TeamID'] = $_GET['TeamID'];
+        
+                $stmt = $conn->prepare($sql);
+                $stmt->execute($namedParameters);
+        
+        
+        /* Here we delete the employee from the employeeteam table WHERE THEY ARE MEMBER */
+                $sql = "DELETE FROM employeeteam
+                        WHERE EmployeeID  = :EmployeeID
+                        AND TeamID = :TeamID";
+        
+                //Sanitizing Input
+                $namedParameters = array();
+                $namedParameters[':EmployeeID'] = $_GET['EmployeeID'];
+                $namedParameters[':TeamID'] = $ParentNode;
+        
+                $stmt = $conn->prepare($sql);
+                $stmt->execute($namedParameters);
+        } else {
+                //Where employee is NOT the manager of the team
+                $sql = "SELECT ResourceID FROM teamresource WHERE TeamID = :TeamID";
+                
+                $namedParameters = array();
+                $namedParameters[':TeamID'] = $_GET['TeamID'];
+                $stmt = $conn->prepare($sql);
+                $stmt->execute($namedParameters);
+                $resourceID = array();
+        
+                while( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+                        $resourceID[] = $row; // appends each row to the array. 
+                }
+                
+                $sql = "DELETE FROM employeeresource
+                        WHERE EmployeeID = :EmployeeID 
+                        AND AccessID = 2 
+                        AND (";
+                
+                for($i = 0; $i < count($resourceID); $i++){
+                        $sql = $sql . " ResourceID = " . $resourceID[$i]["ResourceID"];
+                        if($i < count($resourceID) - 1) {
+                                $sql = $sql . " OR ";
+                        } elseif($i == count($resourceID) - 1){
+                                $sql = $sql . ")";
+                        }
+                }
+                
+                $namedParameters = array();
+                $namedParameters[':EmployeeID'] = $_GET['EmployeeID'];
+        
+                $stmt = $conn->prepare($sql);
+                $stmt->execute($namedParameters);
+                
+                /* Here we delete the employee from the employeeteam table */
+                $sql = "DELETE FROM employeeteam
+                        WHERE EmployeeID  = :EmployeeID
+                        AND TeamID = :TeamID";
+        
+                //Sanitizing Input
+                $namedParameters = array();
+                $namedParameters[':EmployeeID'] = $_GET['EmployeeID'];
+                $namedParameters[':TeamID'] = $_GET['TeamID'];
+        
+                $stmt = $conn->prepare($sql);
+                $stmt->execute($namedParameters);
+        }
+} else {
+  echo false;
+}
 
 ?>
