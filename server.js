b41108d46fec0e2d446aa6bc61bfb0536b43b115
Brandon <bavery@csumb.edu>
2018-05-08 08:30:31 +0000

    Fixed team list on profile page

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ io.on('connection', function(client) {
    
     axios({
       method: 'get',
-      url: "http://capstone-intel-maveyma.c9users.io:8080/capstone-intel-2018/dist/API/DisplayUsers.php",
+      url: "http://cst499s18-bavery.c9users.io:8080/capstone-intel-2018/dist/API/DisplayUsersTeam.php",
       })
       .then(function (response) {
          var info = [response.data];
@@ -79,7 +79,7 @@ io.on('connection', function(client) {
     
     axios({
       method: 'get',
-      url: "http://capstone-intel-maveyma.c9users.io:8080/capstone-intel-2018/dist/API/DisplayEmployeeInfo.php",
+      url: "http://cst499s18-bavery.c9users.io:8080/capstone-intel-2018/dist/API/DisplayEmployeeInfo.php",
       params: { "EmployeeID": num }
     })
     .then(function (response) {
