7438af9a67d0c3eca9b187f83df1e6c26d875fb6
Mavey Ma <mma@csumb.edu>
2018-05-10 06:19:10 +0000

    GetTeamByID.php just grabs one team

---
+++ dist/API/GetTeamByID.php
@@ -0,0 +1,25 @@
+<?php
+header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN'] . "");
+include 'dbConnection.php';
+
+
+$conn = getDatabaseConnection();
+
+$sql = "SELECT *
+        FROM team
+        WHERE TeamID = :TeamID";
+
+//Sanitizing Input
+$namedParameters = array();
+$namedParameters[':TeamID'] = $_GET['TeamID'];
+        
+$stmt = $conn->prepare($sql);
+$stmt->execute($namedParameters);
+$rows = array();
+
+while( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+  $rows[] = $row; // appends each row to the array
+}
+
+echo json_encode($rows);
+?>
