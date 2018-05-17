a4c3759c4daf2ba7966d2d8f1c1bc997ba691767
Jaime Velazquez <javelazquez@csumb.edu>
2018-05-17 00:03:17 +0000

    Team-less employees now display in search

--- dist/API/DisplayUsersTeam.php
+++ dist/API/DisplayUsersTeam.php
@@ -7,7 +7,8 @@ include 'dbConnection.php';
 
 $conn = getDatabaseConnection();
 
-$sql = "SELECT employee.EmployeeID, employee.Name, employee.Phone, employee.Email, employee.isManager, team.TeamName, employeeteam.TeamID
+$sql = "SELECT employee.EmployeeID, employee.Name, employee.Phone, employee.Email, employee.isManager, 
+        team.TeamName, employeeteam.TeamID, employeeteam.isTeamManager
         FROM employee LEFT JOIN employeeteam ON (employee.EmployeeID = employeeteam.EmployeeID)
         LEFT JOIN team ON (employeeteam.TeamID = team.TeamID)
         WHERE isManager = 0 OR isTeamManager = 1 OR (isTeamManager IS NULL AND isManager = 1)";
